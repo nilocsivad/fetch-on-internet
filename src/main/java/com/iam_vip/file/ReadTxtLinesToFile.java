a4e5525872717b6b310195c09481835a79f4fa0c
dfs-1202 <dfs-1202@windows10.microdone.cn>
2018-12-11 11:06:11 +0800

    PNG合并为PNG质量更高,jpg合并为jpg质量更高；

---
+++ src/main/java/com/iam_vip/file/ReadTxtLinesToFile.java
@@ -0,0 +1,118 @@
+package com.iam_vip.file;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ * 
+ */
+
+/**
+ * @author Administrator
+ */
+public class ReadTxtLinesToFile {
+
+	/**
+	 * 
+	 */
+	public ReadTxtLinesToFile() {
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		
+		String txt_path = "xxxxxxxxxxxxx.txt";
+		
+		
+		File txt_file = new File(txt_path);
+		
+		//ReadSeveralLines( txt_file, 0, 10000 );
+
+		SplitTo( txt_file, 1000000 );
+
+	}
+	
+	/**
+	 * @param txt_file
+	 * @param max_lines must greater than 0
+	 */
+	public static void SplitTo( File txt_file, int max_lines ) throws Exception {
+		
+		max_lines = max_lines < 1 ? 1 : max_lines;
+		
+		BufferedReader reader = new BufferedReader( new FileReader( txt_file ) );
+
+		String line = null;
+		FileWriter writer = null;
+		int i = 0;
+		
+		while ( ( line = reader.readLine() ) != null ) {
+			
+			
+			if ( i == 0 ) {
+				
+				if ( writer != null ) {
+					writer.flush();
+					writer.close();
+				}
+				String to = txt_file + "_" + System.currentTimeMillis();
+				writer = new FileWriter( to );
+				System.out.println( "To: " + to );
+				Thread.sleep(1);
+			}
+
+			writer.write( line + "\r\n" );
+			i++;
+			
+			if ( i == max_lines ) {
+				i = 0;
+			}
+			
+		}
+
+		if ( writer != null ) {
+			writer.flush();
+			writer.close();
+		}
+		
+		reader.close();
+	}
+
+	/**
+	 * @param txt_file
+	 * @param min greater than 0
+	 */
+	public static void ReadSeveralLines( File txt_file, int min, int max ) throws Exception {
+		
+		BufferedReader reader = new BufferedReader( new FileReader( txt_file ) );
+		
+		FileWriter writer = new FileWriter( txt_file.getAbsolutePath() + "_" + System.currentTimeMillis() );
+
+		String line = null;
+		int i = 0;
+		
+		while ( ( line = reader.readLine() ) != null ) {
+			
+			i++;
+			
+			if ( i >= min && i <= max ) {
+				writer.write( line + " \r\n");
+			}
+			
+			if ( i >= max ) {
+				writer.flush();
+				break;
+			}
+			
+		}
+		
+		reader.close();
+		writer.close();
+	}
+
+}
--- src/main/java/com/iam_vip/img/MergeImage.java
+++ src/main/java/com/iam_vip/img/MergeImage.java
@@ -5,10 +5,16 @@ package com.iam_vip.img;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
 
 import org.junit.Test;
 
@@ -32,45 +38,148 @@ public class MergeImage {
 			
 		//MergeEnglish1();
 			
-		MergePPT1();	
+		//MergePPT1();	
+		
+		//MergeByV( "" );
 		
 	}
 	
 	
 	
 	/**
+	 * /// 自动按时间顺序排序,之后按给出的最大高度合并 ///
 	 * @throws IOException
 	 * @throws InterruptedException 
 	 */
-	public void MergePPT1() throws IOException, InterruptedException {
+	public void MergeByV( String directory ) throws IOException, InterruptedException {
+		this.MergeByV(directory, null);
+	}
+	
+	/**
+	 * /// 自动按时间顺序排序,之后按给出的最大高度合并 ///
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 */
+	public void MergeByV( String directory, FilenameFilter filter ) throws IOException, InterruptedException {
 		
-		File src_f = new File( "D:\\University-English-Others\\通用英语1-姜芸\\通用1-U4-PPT-截图\\" );
+		File src_f = new File( directory );
 		System.out.println( "Merge the pictures from " + src_f.getAbsolutePath() );
 		
-		File[] imgs = src_f.listFiles( ( f, n ) -> {
-			return f.isDirectory() && n.endsWith( ".png" );
-		});
+		/// 扫描图片文件 ///
+		File[] fs = filter == null ? src_f.listFiles() : src_f.listFiles( filter );
+		int len = fs.length;
 		
-		int len = imgs.length;
+		String formatName = suffix( fs[0] );
 		
-		BufferedImage[] images = new BufferedImage[ len ];
-		int max_width = 0;
-		int max_height = 0;
-	
-		for ( int j = 0; j < len; ++j ) {
-			
-				BufferedImage cur_img = ImageIO.read( imgs[j] );
-				images[j] = cur_img;
-				
-				int cur_w = cur_img.getWidth();
-				int cur_h = cur_img.getHeight();
-				
+		/// 初始化图片集合 ///
+		BufferedImage[] images = null; // new BufferedImage[ len ];
+		int max_width = 0;   /// 所有图片的最宽的图片像素 ///
+		int max_height = 0;  /// 所有图片的最高的图片像素 ///
+		int full_height = 0; /// 所有图片的高度总合 ///
+		
+		{ /// 转换文件为图片对象 ///
+			List<File> list = Arrays.asList( fs );
+			Collections.sort( list, new Comparator<File>() { /// 按照时间进行排序 ///
+				public int compare(File o1, File o2) { return (int) (o1.lastModified() - o2.lastModified()); }
+			});
+			/// 加载文件并转换为图片对象 ///
+			List<BufferedImage> temporary = new ArrayList<>( len + 1 );
+			for ( File itm : list ) {
+				BufferedImage img0 = ImageIO.read( itm );
+				if ( img0 == null ) continue;
+				temporary.add( img0 );
+				int cur_w = img0.getWidth();
+				int cur_h = img0.getHeight();
+				full_height += cur_h;
 				max_width = cur_w > max_width ? cur_w : max_width;
 				max_height = cur_h > max_height ? cur_h : max_height;
+			}
+			images = temporary.toArray( new BufferedImage[0] );
 		}
 		
+		String content = JOptionPane.showInputDialog( "The height of the files in this directory is " 
+				+ full_height + " How tall is for per picture? (A number, please!)", 1000 );
+		int height = Integer.parseInt( content );
 		
-		int include = 100;
+		/// 要合并的图片临时集合 ///
+		List<BufferedImage> temporary = new ArrayList<>( 20 );
+		
+		int tmp_h = 0; /// 计算图片高度临时变量 ///
+		
+		for ( BufferedImage img : images ) {
+			
+			tmp_h += img.getHeight();
+			temporary.add( img );
+			
+			if ( height > 0 && tmp_h > height ) { /// 达到某个高度后开始合并操作 ///
+				
+				File to = new File( src_f, src_f.getName() + "-merge-" + DTUtil.formatA(2) + "." + formatName );
+				System.out.println( to.getAbsolutePath() );
+				
+				BufferedImage save_img = mergeVertical( 40, 40, temporary.toArray( new BufferedImage[0] ) );
+				ImageIO.write( save_img, formatName, to );
+				
+				temporary.clear();
+				tmp_h = 0;
+				
+				Thread.sleep(1000);
+			}
+		}
+		
+		/// 若存在最后未达到高度的图片,则仍要合并 ///
+		if ( temporary.isEmpty() == false && temporary.size() > 0 ) {
+			
+			File to = new File( src_f, src_f.getName() + "-merge-" + DTUtil.formatA(2) + "." + formatName );
+			System.out.println( to.getAbsolutePath() );
+			
+			BufferedImage save_img = mergeVertical( 40, 40, temporary.toArray( new BufferedImage[0] ) );
+			ImageIO.write( save_img, formatName, to );
+		}
+		
+	}
+	
+	
+	/**
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 */
+	public void MergePPT1( String dir ) throws IOException, InterruptedException {
+		
+		File src_f = new File( dir );
+		System.out.println( "Merge the pictures from \r\n" + src_f.getAbsolutePath() );
+		
+		int len = 0;
+		List<BufferedImage> bufs = new ArrayList<>( 150 );
+		{
+			File[] imgs = src_f.listFiles( ( f, n ) -> {
+				return f.isDirectory() && n.endsWith( ".png" );
+			});
+			
+			len = imgs.length;
+			
+			if ( len == 0 ) {
+				System.err.println( "The folder contains 0 files." );
+				return;
+			}
+			
+			List<File> list = Arrays.asList( imgs );
+			Collections.sort( list, ( f1, f2 ) -> {
+				///String s1 = f1.getName().split( "at" )[1].replace( " ", "" ).replace( ".", "" );
+				///String s2 = f2.getName().split( "at" )[1].replace( " ", "" ).replace( ".", "" );
+				///return s1.compareTo( s2 );
+				String[] a1 = f1.getName().replace( ".png", "" ).split( "-" );
+				String[] a2 = f2.getName().replace( ".png", "" ).split( "-" );
+				return Integer.parseInt( a1[ a1.length - 1 ] ) - Integer.parseInt( a2[ a2.length - 1 ] );
+			});
+			
+			for ( File f : list ) {
+				bufs.add( ImageIO.read( f ) );
+			}
+		}
+		
+		BufferedImage[] images = bufs.toArray( new BufferedImage[0] );
+		
+		int include = 10;
 		if ( include > len ) include = len;
 		int group_len = len % include == 0 ? len / include : len / include + 1;
 		
@@ -78,17 +187,17 @@ public class MergeImage {
 				
 				File to = new File( src_f, src_f.getName() + "-merge-" + DTUtil.formatAS(2) + ".jpg" );
 				
-				BufferedImage[] buf = Arrays.copyOfRange( images, k * include, k * include + include );
+				BufferedImage[] buf = Arrays.copyOfRange( images, k * include, k * include + ( k == group_len - 1 ? ( len % include == 0 ? include : len % include ) : include ) );
 				
 				///BufferedImage save_img = mergeHorizontal( max_width, max_height, buf );
-				BufferedImage save_img = mergeVertical( max_width, max_height, buf );
+				BufferedImage save_img = mergeVertical( 30, 60, buf );
 				///BufferedImage save_img = mergeDoubleVertical( max_width, 160, max_height, 0, buf );
 				
 				ImageIO.write( save_img, "JPG", to );
 				
-				System.out.println( to.getAbsolutePath() );
+				System.out.println( "Then save at \r\n" + to.getAbsolutePath() );
 				
-				Thread.sleep(1);
+				Thread.sleep(1000);
 		}
 		
 	}
@@ -96,12 +205,14 @@ public class MergeImage {
 
 
 	/**
+	 * /// 合并两层文件夹中的图片到指定的文件夹中 ///
 	 * @throws IOException
 	 */
 	public void MergeEnglish1() throws IOException {
 		
-		String image_folder = "Y:\\University-Class1-to-6-Words";
-		String to_folder = "Y:\\University-Class1-to-6-Words-2";
+		String image_folder = "";
+		String to_folder = "";
+		
 		String[] suffix = { ".jpg", ".png", ".bmp" };
 		
 		File[] folders = new File( image_folder ).listFiles();
@@ -214,32 +325,26 @@ public class MergeImage {
 	
 	/**
 	 * /// 图片纵向上下顺序合并 ///
-	 * @param img_w 图片宽度
-	 * @param img_h 图片高度
+	 * @param add_w 每张图片左右留白宽度
+	 * @param add_h 每张图片上下留白高度
 	 * @param images 要合并的图片数组
 	 * @return 合成后的图片
 	 */
-	public BufferedImage mergeVertical( int img_w, int img_h, BufferedImage... images ) {
-		return mergeVertical(img_w, 10, img_h, 10, images);
-	}
-	
-	/**
-	 * /// 图片纵向上下顺序合并 ///
-	 * @param img_w 图片宽度
-	 * @param add_w 图片左右留白宽度
-	 * @param img_h 图片高度
-	 * @param add_h 图片上下留白高度
-	 * @param images 要合并的图片数组
-	 * @return 合成后的图片
-	 */
-	public BufferedImage mergeVertical( int img_w, int add_w, int img_h, int add_h, BufferedImage... images ) {
+	public BufferedImage mergeVertical( int add_w, int add_h, BufferedImage... images ) {
+		
+			/// 单个图片的最大宽度 ///
+			int max_w = 0;
 		
 			/// 最终合成的图片宽度与高度 ///
-			int final_w = img_w + add_w, final_h = 0;
+			int final_h = 0;
 			
 			for ( BufferedImage img0 : images ) {
 				final_h += img0.getHeight() + add_h;
+				max_w = ( img0.getWidth() > max_w ) ? img0.getWidth() : max_w;
 			}
+			
+			/// 合成图片后的最终宽度 ///
+			int final_w = max_w + add_w;
 			
 			/// 最终合成的图片对象，并初始化为白色背景 ///
 			BufferedImage final_img = new BufferedImage( final_w, final_h, BufferedImage.TYPE_INT_RGB );
@@ -253,7 +358,7 @@ public class MergeImage {
 			/// 当前写图片到了横纵坐标的多少像素 ///
 			int calc_h = 0;
 			
-			int half_add_h = add_h / 2, half_add_w = add_w / 2;
+			int half_add_h = add_h / 2;
 			
 			for ( BufferedImage img0 : images ) {
 					
@@ -263,7 +368,12 @@ public class MergeImage {
 					int[] buf = new int[ img0_w * img0_h ];
 					buf = img0.getRGB( 0, 0, img0_w, img0_h, buf, 0, img0_w );
 					
-					final_img.setRGB( half_add_w, calc_h + half_add_h, img0_w, img0_h, buf, 0, img0_w );
+					int startX = 0;
+					if ( max_w > img0_w ) {
+						startX = ( max_w - img0_w + add_w ) / 2;
+					}
+					
+					final_img.setRGB( startX, calc_h + half_add_h, img0_w, img0_h, buf, 0, img0_w );
 				
 					calc_h += img0_h + add_h;
 			}
@@ -314,7 +424,14 @@ public class MergeImage {
 	}
 	
 	
+	public static final String suffix( File f ) {
+		return suffix( f.getName() );
+	}
 	
+	public static final String suffix( String n ) {
+		int i = n.lastIndexOf( "." );
+		return n.substring( i + 1 );
+	}
 	
 	
 
